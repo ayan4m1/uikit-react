92b0a8c9a1e101656ca3eca9534aea887863a6a1
ayan4m1 <andrew@bulletlogic.com>
2019-04-01 00:40:00 -0400

    remove unused code

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,7 +1,4 @@
 import * as React from 'react'
-import * as _classNames from 'classnames'
-const classNames = _classNames
-import { setClassNames } from '../../utils/set-class-names'
 
 export class Image extends React.Component<ImageProps, any> {
   render() {
